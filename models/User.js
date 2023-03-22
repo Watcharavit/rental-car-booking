4b269d8cd7523598def0b289e7c00abe72bb2819
NonRoute <chanon.ratn@gmail.com>
2023-03-22 21:21:13 +0700

    Fix response time longer than 3 seconds

--- models/User.js
+++ models/User.js
@@ -42,7 +42,7 @@ const UserSchema = new mongoose.Schema({
 
 //Encrypt password using bcrypt
 UserSchema.pre("save", async function (next) {
-	const salt = await bcrypt.genSalt(16);
+	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
 });
 
