5b2c1ceafe48cb1ab8d4a985ef4b6d043926a1db
watcharavit <watcha.ea@hotmail.com>
2023-04-06 17:16:12 +0700

    add security

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express")
 const dotenv = require("dotenv")
 const cookieParser = require("cookie-parser")
 const connectDB = require("./config/db")
-const morgan = require('morgan')
+const morgan = require("morgan")
+const mongoSanitize = require("express-mongo-sanitize")
+const helmet = require("helmet")
+const xss = require("xss-clean")
+const rateLimit = require("express-rate-limit")
+const hpp = require("hpp")
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" })
@@ -23,8 +28,22 @@ app.use(express.json())
 //Cookie parser
 app.use(cookieParser())
 
+app.use(mongoSanitize())
+// Set security header
+app.use(helmet())
+// prevent xss(cross site scripting) attacks
+app.use(xss())
+// rate limit
+const limiter = rateLimit({
+	windowsMs: 10 * 60 * 1000, //10 mins
+	max: 50 // amount
+})
+app.use(limiter)
+//Prevent http param pollutions
+app.use(hpp())
+
 //Morgan - HTTP request logger
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 
 //Mount routers
 app.use("/provider", provider)
